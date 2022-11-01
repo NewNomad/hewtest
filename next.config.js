ab95499f41fc0e9ec12500caac0a5177087a6632
NewNomad <tosiki1997@gmail.com>
2022-11-01 10:54:59 +0900

    環境構築

--- next.config.js
+++ next.config.js
@@ -5,3 +5,19 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  env: {
+    MYSQL_HOST: "127.0.0.1",
+    MYSQL_DATABASE: "dnpk_dnpk_oes",
+    MYSQL_USER: "root",
+    MYSQL_PASSWORD: "root", // 中尾専用
+    MYSQL_PORT: 8889 // 中尾専用
+  },
+  // webpack: {
+  //   target: "node"
+  // },
+  webpack: {
+    target: "node"
+  },
+}
