65604185dcae51e77e67f9b3703093341ca50be9
jonny0625 <nhs15015@gmail.com>
2022-11-03 08:48:41 +0900

    時間表示
    React.Componentを継承する形で調整中
    
    Signed-off-by: jonny0625 <nhs15015@gmail.com>

--- src/components/1atoms/ShowDate.tsx
+++ src/components/1atoms/ShowDate.tsx
@@ -1,40 +1,36 @@
-import { Typography }   from '@mui/material'
-import { Container }    from '@mui/system'
+import { Container }    from '@mui/system'          // コンテナ
+import { Typography }   from '@mui/material'        // テキスト(書式未設定)
 
-import React, { useState, useEffect }  from 'react'
+import React from 'react'
 
 
 // ===================================================
-// 時間表示
+// ヘッダー 時間表示
 // ===================================================
 
-// 解決でき次第、TODOは削除してください
-// TODO:更新の様子が微妙に怪しい(おそらく処理が重すぎる)
-// TODO:二桁表記未対応
+export default class ShowDate extends React.Component {
 
-export const ShowDate = () => {
+    constructor(props) {
+        super(props);
+        this.state = { current: new Date().toLocaleString() };
+    }
 
-    var nowDate = new Date();
-
-    const [nowHour, setNowHour] = useState(nowDate.getHours().toString());
-    const [nowMin, setNowMin]   = useState(nowDate.getMinutes().toString());
-    const [nowSec, setNowSec]   = useState(nowDate.getSeconds().toString());
-
-    useEffect( () => {
-        var nowNewDate = new Date();
-        setInterval( ()=> 
-        {
-            setNowHour(nowNewDate.getHours().toString());
-            setNowMin(nowNewDate.getMinutes().toString());
-            setNowSec(nowNewDate.getSeconds().toString());
-        }, 1000)
-    });
-
-    return (
-        <Container>
-            <Typography variant='h4' textAlign="center">
-                { nowHour }:{ nowMin }:{ nowSec }
-            </Typography>
-        </Container>
-    )
+    render(){
+        return(
+            <Container>
+                <Typography variant='h4' textAlign="center">{ this.state.current }</Typography>
+            </Container>
+        );
+    }
 }
+
+// export const ShowDate = () => {
+//     return (
+//         <Container>
+//             <Typography variant='h4' textAlign="center">
+//                 { nowHour }:{ nowMin }:{ nowSec }
+//             </Typography>
+//         </Container>
+//     );
+
+
--- src/components/2molecules/Header.tsx
+++ src/components/2molecules/Header.tsx
@@ -3,19 +3,19 @@ import { ShowDate }         from '../1atoms/ShowDate'
 
 import React from 'react'
 
+
 // ===================================================
 // ヘッダー
 // ===================================================
 export const Header = () => {
   return (
-    <AppBar
-      position='fixed'
-      color='primary'
-      // sx={{ borderBottom: (t) => `1px solid #ccc` }}
-      >
-      <Toolbar>
-          <ShowDate />
-      </Toolbar>
+    <AppBar position='fixed'
+            color='primary'
+            // sx={{ borderBottom: (t) => `1px solid #ccc` }}
+    >
+        <Toolbar>
+            <ShowDate />
+        </Toolbar>
     </AppBar>
   )
 }
