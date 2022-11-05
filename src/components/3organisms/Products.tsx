978180bc6cf84c099542c8ca16c3f9d00c23dee6
NewNomad <tosiki1997@gmail.com>
2022-11-05 21:38:55 +0900

    商品陳列製作中

--- src/components/3organisms/Products.tsx
+++ src/components/3organisms/Products.tsx
@@ -1,4 +1,5 @@
-import { Container, Grid, Paper } from '@mui/material'
+import { Button, Container, Grid, Paper } from '@mui/material'
+import { width } from '@mui/system'
 import Image from 'next/image'
 import React from 'react'
 
@@ -8,22 +9,21 @@ export const Products = () => {
         <Grid container alignItems="center" spacing={0.5} padding={1}>
             {[...Array(12)].map((e) => (
                 <Grid item xs={1}>
-                    <Paper elevation={0} variant="outlined" key={0}
+                    <Paper elevation={0}  variant="outlined" key={0}
                         sx={{
-                            borderRadius: 5,
+                            borderRadius: 3,
                             backgroundColor: "rgba(0,0,0,0)",
                             border: "3px solid #FFF"
                         }}>
                         <Image src="" height={300} width={200} objectFit="contain"></Image>
-                        <Container
+                        <Button color='secondary' variant='contained' disableElevation
                             sx={{
-                                backgroundColor: "red",
-                                borderRadius: 5
-                            }}
-                        >
+                                borderRadius: 2,
+                                width: 1,
+                            }}>
                             140円
-                        </Container>
 
+                        </Button>
                     </Paper>
                 </Grid>
             )
