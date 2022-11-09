b471c8ac76611c4ca4443d6ac11c5bf89b9aff5a
NewNomad <tosiki1997@gmail.com>
2022-11-09 15:44:08 +0900

    エラーを修正

--- src/components/2molecules/ProductTabs.tsx
+++ src/components/2molecules/ProductTabs.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export const ProductTabs = () => {
     return (
-        <Tabs value={null} onChange={() => null} centered variant='fullWidth' sx={{ bgColor: "secondary" }}>
+        <Tabs value={0} onChange={() => null} centered variant='fullWidth' sx={{ bgColor: "secondary" }}>
             <Tab icon={<LocalDrink />} label="商品1" iconPosition='start' />
             <Tab icon={<LocalDrink />} label="商品2" iconPosition='start' />
             <Tab icon={<LocalDrink />} label="商品3" iconPosition='start' />
