ba06df9d56467c8af34aaf32690068b108389bda
NewNomad <tosiki1997@gmail.com>
2022-11-30 06:52:27 +0900

    ShowInfo遷移ボタン追加

---
+++ src/components/1atoms/BtnShowInfo.tsx
@@ -0,0 +1,16 @@
+import { Info } from '@mui/icons-material'
+import { Button } from '@mui/material'
+import React from 'react'
+
+type Props = {
+    link: string,
+    name: string
+}
+
+export const BtnShowInfo = ({ link, name }: Props) => {
+    return (
+        <Button startIcon={<Info />} variant="contained" color="primary" size='large' disableElevation href={link}>
+            {name}
+        </Button>
+    )
+}
--- src/components/2molecules/Header.tsx
+++ src/components/2molecules/Header.tsx
@@ -1,22 +1,32 @@
-import { AppBar,  Toolbar }   from '@mui/material'
-import { ShowDate }           from '../1atoms/ShowDate'
-import { ShowWeather }        from '../1atoms/ShowWeather'
+import { AppBar, Button, Toolbar } from '@mui/material'
+import { ShowDate } from '../1atoms/ShowDate'
+import { ShowWeather } from '../1atoms/ShowWeather'
 import React from 'react'
+import { Info } from '@mui/icons-material';
+import { useRouter } from 'next/router';
+import { BtnShowInfo } from '../1atoms/BtnShowInfo';
+
+
 
 // 自販機の設置場所
-const machinePlace:string = "名古屋";
+const machinePlace: string = "名古屋";
 
 // ===================================================
 // ヘッダー
 // ===================================================
 export const Header = () => {
+  const router = useRouter();
 
   return (
     <AppBar position='fixed' color='secondary'>
-        <Toolbar>
-            <ShowDate />
-            <ShowWeather place={machinePlace} />
-        </Toolbar>
+      <Toolbar>
+        <ShowWeather place={machinePlace} />
+        <ShowDate />
+        {router.pathname != "/ShowInfo"
+          ? <BtnShowInfo link='/ShowInfo' name='地図を見る' />
+          : <BtnShowInfo link='/' name='メイン画面に戻る' />
+        }
+      </Toolbar>
     </AppBar>
   )
 }
