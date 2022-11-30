2f1abb2ea742f1db92257b0ada88859b6dea49bd
jonny0625 <nhs15015@gmail.com>
2022-11-30 17:11:21 +0900

    金額表示 リファクタリング
    [ShowPrice.tsx] ソースコード整理
    
    Signed-off-by: jonny0625 <nhs15015@gmail.com>

--- src/components/1atoms/ShowPrice.tsx
+++ src/components/1atoms/ShowPrice.tsx
@@ -1,9 +1,12 @@
-import { Grid, Typography }   from '@mui/material'
+import { Typography }   from '@mui/material'
 
 // ================================================
 // 金額テキスト
 // ================================================
 export const ShowPrice = ({children, primary = false}) =>{
-
-    return <Typography variant='h6' color={primary?'primary':'#000'}>{ children }円</Typography>
+    return <Typography
+                variant='h6'
+                color={primary?'primary':'#000'}>
+                { children }円
+            </Typography>
 }
