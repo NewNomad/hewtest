5b48f631e45b217ba2e528648cf171ff0cf99317
aaaa <pocchaman0393@gmail.com>
2022-11-30 16:29:31 +0900

    map

--- src/pages/ShowInfo.tsx
+++ src/pages/ShowInfo.tsx
@@ -1,9 +1,19 @@
-import { Box, Typography, Button, Grid }                  from '@mui/material'
-import { HeadInfo }             from '../components/2molecules/HeadInfo'
-import { Header }               from '../components/2molecules/Header'
-import { Footer }               from '../components/2molecules/Footer'
+import React from "react";
+import GoogleMapReact from 'google-map-react';
+import { Box, Typography, Button, Grid } from '@mui/material'
+import { HeadInfo } from '../components/2molecules/HeadInfo'
+import { Header } from '../components/2molecules/Header'
+import { Footer } from '../components/2molecules/Footer'
 
-export default function Advertisment(){
+export default function Advertisment() {
+    const defaultProps = {
+        center: {
+            lat: 35.1709,
+            lng: 136.8815
+
+        },
+        zoom: 11
+    };
     return (
         <>
             <HeadInfo title='mopacal | 地図' />
@@ -12,14 +22,24 @@ export default function Advertisment(){
                 <Header />
 
                 <Box sx={{ pt: 8 }}>
-                    <Box sx={{width: '100%', height: 800, backgroundColor: 'primary.main', opacity: 0.6}}>
-                        <Typography>地図表示</Typography>
-                        [ここに画像]
+                    <Box sx={{ width: '100%', height: 800, backgroundColor: 'primary.main', opacity: 0.6 }}>
+                        {/*Important! Always set the container height explicitly */}
+                        <div style={{ height: '100vh', width: '100%' }}>
+                            <GoogleMapReact
+                                bootstrapURLKeys={{ key: "AIzaSyBAzmp9Vpi5MsOnuYLSq1Ix_sq8vnuSHpU" }}
+                                defaultCenter={defaultProps.center}
+                                defaultZoom={defaultProps.zoom}
+                            >
+                                {/* <AnyReactComponent
+                                    lat={59.955413}
+                                    lng={30.337844}
+                                    text="My Marker"
+                                /> */}
+                            </GoogleMapReact>
+                        </div>
+                        );
                     </Box>
                 </Box>
-
-                <Footer />
-
             </Box>
 
         </>
