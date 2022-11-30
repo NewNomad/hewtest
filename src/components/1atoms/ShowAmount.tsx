c1e16298bb0e9dcc2884b55b555b3860807c5840
jonny0625 <nhs15015@gmail.com>
2022-11-30 12:50:19 +0900

    入金画面 リファクタリング

--- src/components/1atoms/ShowAmount.tsx
+++ src/components/1atoms/ShowAmount.tsx
@@ -1,6 +1,5 @@
 import { AddCircle, RemoveCircle }          from '@mui/icons-material'
 import { IconButton, Grid, Typography }     from '@mui/material'
-import { ShowPrice }                        from '../1atoms/ShowPrice'
 
 export const ShowAmount = ({costs, ClickMinus, ClickPlus}) =>{
     return (
