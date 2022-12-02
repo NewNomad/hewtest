8731b7062a73105b15c615bda7f82211cd2e67c2
NewNomad <tosiki1997@gmail.com>
2022-12-02 18:26:36 +0900

    在庫管理sql分

--- src/pages/CompPay.tsx
+++ src/pages/CompPay.tsx
@@ -1,11 +1,25 @@
-import { Box, Typography, Paper, Button, Grid }    from '@mui/material'
-import { TextTitle }    from '../components/1atoms/TextTitle'
-import { HeadInfo }     from '../components/2molecules/HeadInfo'
-import { Header }       from '../components/2molecules/Header'
-import { LinkButton }   from '../components/1atoms/BtnLink'
-import Link             from 'next/link'
+import { Box, Typography, Paper, Button, Grid, Backdrop } from '@mui/material'
+import { TextTitle } from '../components/1atoms/TextTitle'
+import { HeadInfo } from '../components/2molecules/HeadInfo'
+import { Header } from '../components/2molecules/Header'
+import { LinkButton } from '../components/1atoms/BtnLink'
+import Link from 'next/link'
+import useSWR from 'swr'
 
-export default function CheckPay(){
+const updateStock = "/api/updateStock"
+const fetcher = (url: string) => fetch(url).then(response => response.json());
+
+export default function CheckPay() {
+
+    const { data, error } = useSWR(updateStock, fetcher);
+
+    if (!data) return (<Backdrop
+        sx={{
+            color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1
+        }}
+        open={true}></Backdrop>)
+    if (error) return ("エラーです")
+
     return (
         <>
             <HeadInfo title='mopacal | 入金完了' />
@@ -13,10 +27,10 @@ export default function CheckPay(){
             <Box sx={{ flexGrow: 1 }}>
                 <Header />
 
-                <Box textAlign="center" sx={{ margin:5, padding:10, pt: 8 }}>
+                <Box textAlign="center" sx={{ margin: 5, padding: 10, pt: 8 }}>
                     <TextTitle>ありがとうございました！</TextTitle>
 
-                    <Box sx={{width: '100%', height: 600, padding: 10, backgroundColor: 'primary.main'}}>
+                    <Box sx={{ width: '100%', height: 600, padding: 10, backgroundColor: 'primary.main' }}>
                         [ここに画像]
                     </Box>
 
---
+++ src/pages/api/updateStock.tsx
@@ -0,0 +1,50 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/Home.module.css'
+
+
+import mysql from "serverless-mysql"
+import { log } from 'console';
+
+const db = mysql({
+  config: {
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    port: 8889, //中尾専用
+  }
+})
+
+exports.query = async (query: any) => {
+  try {
+    const results = await db.query(query)
+    await db.end()
+    return results
+  } catch (error) {
+    return error
+  }
+
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+
+  // console.log(req);
+
+  let sql = ""
+
+
+  const result = await db.query(`UPDATE
+  t_products
+  SET
+  f_product_stock = ${req.query.stock}
+  WHERE
+  f_product_id = ${req.query.id}
+  `);
+  return res.status(200).json(result)
+}
