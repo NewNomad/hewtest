98adbe529a5045ffacc445c81d26c632989acde3
aaaa <pocchaman0393@gmail.com>
2022-12-03 23:18:27 +0900

    動画広告追加

--- src/pages/ShowAdvertisment.tsx
+++ src/pages/ShowAdvertisment.tsx
@@ -3,7 +3,7 @@ import { HeadInfo }             from '../components/2molecules/HeadInfo'
 import { Header }               from '../components/2molecules/Header'
 import { LinkButton }           from '../components/1atoms/BtnLink'
 
-export default function ShowAdvertisment(){
+export default function ShowAdvertisment() {
     return (
         <>
             <HeadInfo title='mopacal | 広告' />
@@ -12,9 +12,14 @@ export default function ShowAdvertisment(){
                 <Header onMap/>
 
                 <Box sx={{ pt: 8 }}>
-                    <Box sx={{width: '100%', height: 800, backgroundColor: 'primary.main', opacity: 0.6}}>
-                        <Typography>広告表示</Typography>
-                        [ここに画像]
+                    <Box sx={{ width: '100%', height: 800, backgroundColor: 'primary.main', opacity: 1.0 }}>
+                        {/* <Typography>広告表示</Typography> */}
+                        <React.StrictMode>
+                            <video controls loop autoPlay muted width="100%" height="100%">
+                                <source src="CM.mp4" type="video/mp4" />
+                                <p>Your browser doesn't support HTML5 video.</p>
+                            </video>
+                        </React.StrictMode>
                     </Box>
                 </Box>
 
