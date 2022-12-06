a72c3494491a26ced8e50857a02277ed25c6d76f
NewNomad <tosiki1997@gmail.com>
2022-12-06 13:03:43 +0900

    gitignoreにnext.config.js追加

--- next.config.js
+++ next.config.js
@@ -10,15 +10,11 @@ module.exports = {
   env: {
     MYSQL_HOST: "127.0.0.1",
 
-    //MYSQL_DATABASE: "hew2023",        // 中尾専用
-    //MYSQL_USER: "root",          // win・mac共通
-    //MYSQL_PASSWORD: "root",      // 中尾専用(mac)
-    //MYSQL_PORT: 8889             // 中尾専用(mac)
+    MYSQL_DATABASE: "hew2023",        // 中尾専用
+    MYSQL_USER: "root",          // win・mac共通
+    MYSQL_PASSWORD: "root",      // 中尾専用(mac)
+    MYSQL_PORT: 8889          // 中尾専用(mac)
 
-    MYSQL_DATABASE: "hew2023",  // 小松専用
-    MYSQL_USER: "root",             // win・mac共通
-    MYSQL_PASSWORD: "",             // 小松専用(win)
-    MYSQL_PORT: 3306                // 小松専用(win)
   },
   webpack: {
     target: "node"
