ee3c17f3bbac5f43820c48f854845f6bdcbc624e
NewNomad <tosiki1997@gmail.com>
2022-12-06 13:57:41 +0900

    モーダルの機能追加

--- src/components/1atoms/ShowModal.tsx
+++ src/components/1atoms/ShowModal.tsx
@@ -1,15 +1,16 @@
 import { Cancel, Close } from '@mui/icons-material'
-import { Paper } from '@mui/material'
+import { IconButton, Paper } from '@mui/material'
 import React from 'react'
 
 type Props = {
     children: React.ReactNode
+    closeModal: () => void
 }
 
 // ==================================================
 // モーダルのウィンドウ設定
 // ==================================================
-export const ShowMordal: React.FC<Props> = ({ children }) => {
+export const ShowMordal: React.FC<Props> = ({ children, closeModal }) => {
     return (
         <Paper
             sx={{
@@ -23,14 +24,17 @@ export const ShowMordal: React.FC<Props> = ({ children }) => {
                 p: 4
             }}
         >
-            <Close sx={{
-                position: "absolute",
-                right:-40,
-                top:-40,
-                fontSize:80,
-                borderRadius:50,
-                backgroundColor:"#fff"
-            }}></Close>
+            <IconButton onClick={closeModal}>
+                <Close sx={{
+                    position: "absolute",
+                    right: -40,
+                    top: -40,
+                    fontSize: 80,
+                    borderRadius: 50,
+                    backgroundColor: "#fff"
+                }}></Close>
+            </IconButton>
+
             {children}
         </Paper>
     )
