faf70cfc876aa68fc4f6a4630ba1da6993d3d890
NewNomad <tosiki1997@gmail.com>
2022-12-09 13:40:49 +0900

    indexの商品画像に何個選択中か表示

--- src/components/2molecules/Product.tsx
+++ src/components/2molecules/Product.tsx
@@ -2,17 +2,18 @@ import { Badge, Button, Divider, Modal, Paper, Tooltip } from '@mui/material'
 import { AcUnit, Whatshot } from '@mui/icons-material'
 import { ShowModalInfo } from './ShowModalInfo'
 import { TypeProducts } from '../types/TypeProducts'
-import { cartState, useCart } from '../types/TypeCart'
+import { cartState, TypeCart, useCart } from '../types/TypeCart'
 import Image from 'next/image'
 import { useRecoilState } from 'recoil'
 import React, { useState } from 'react'
 
 type Props = {
     proinfo: TypeProducts;
+    cart: TypeCart
 }
 
 export default function Product({ proinfo }: Props) {
-    const { id, name, price, stock, isice, imageURL ,quantity} = proinfo
+    const { id, name, price, stock, isice, imageURL } = proinfo
 
     // const TimageURL = imageURL.replace(/(\d{4})\//, "$1/$1")     // DBと名前が違ったので、暫定的におく
 
@@ -26,59 +27,72 @@ export default function Product({ proinfo }: Props) {
 
     const { addCart } = useCart()
 
+    const [cart] = useRecoilState(cartState)
+
+    const quantity = cart.products.find((e, i) => e.id == id)?.quantity
+
     return (
         <>
-            <Badge badgeContent={quantity} color="primary">
-                <Paper elevation={3} variant="elevation" key={0}
+            <Paper elevation={3} variant="elevation" key={0}
+                sx={{
+                    backgroundColor: "#fff",
+                    // border: `2px solid #ccc `,
+                    borderColor: `secondary.main`,
+                    textAlign: "center"
+                }}>
+
+                {/* <Tooltip title="詳細" arrow> */}
+
+                <Button
+                    onClick={OpenMInfo}
+                    disabled={isActive ? false : true}
                     sx={{
-                        backgroundColor: "#fff",
-                        // border: `2px solid #ccc `,
-                        borderColor: `secondary.main`,
-                        textAlign: "center"
+                        width: 1,
+                        borderBottomLeftRadius: 0,
+                        borderBottomRightRadius: 0,
                     }}>
-                    <Tooltip title="詳細" arrow>
-                        <Button
-                            onClick={OpenMInfo}
-                            disabled={isActive ? false : true}
-                            sx={{
-                                width: 1,
-                                borderBottomLeftRadius: 0,
-                                borderBottomRightRadius: 0,
-                            }}>
-                            <Image
-                                src={"/" + imageURL}
-                                height={180}
-                                width={100}
-                                objectFit="contain"
-                                style={isActive == false ? { filter: "grayscale(100%)" } : {}}
-                                alt="商品画像" />
-                        </Button>
-                    </Tooltip>
+                    <Badge badgeContent={quantity} color="primary" max={9} sx={{
+                        position: "relative",
+                        top: -65,
+                        right: -75
+                    }} />
+                    <Image
+                        src={"/" + imageURL}
+                        height={180}
+                        width={100}
+                        objectFit="contain"
+                        style={isActive == false ? { filter: "grayscale(100%)" } : {}}
+                        alt="商品画像" />
 
-                    <Divider />
+                </Button>
+                {/* </Tooltip> */}
 
-                    <Tooltip title="カートに追加" arrow>
-                        <Button color='secondary' variant="text" size='small' disableElevation
-                            onClick={() => addCart(proinfo)}
-                            disabled={isActive ? false : true}
-                            startIcon={
-                                isice == 1
-                                    ? <AcUnit color={isActive ? "info" : "secondary"} />
-                                    : <Whatshot color={isActive ? 'primary' : "secondary"} ></Whatshot>
-                            }
-                            // <Button color='secondary' variant="contained" size='small' disableElevation
-                            sx={{
-                                // borderRadius: 10,
-                                // marginBottom:0.5
-                                width: 1,
-                                borderStartEndRadius: 0,
-                                borderStartStartRadius: 0
-                            }}>
-                            {proinfo.price}
-                        </Button>
-                    </Tooltip>
-                </Paper>
-            </Badge>
+                <Divider />
+
+                <Tooltip title="カートに追加" arrow>
+
+                    <Button color='secondary' variant="text" size='small' disableElevation
+                        onClick={() => addCart(proinfo)}
+                        disabled={isActive ? false : true}
+                        startIcon={
+                            isice == 1
+                                ? <AcUnit color={isActive ? "info" : "secondary"} />
+                                : <Whatshot color={isActive ? 'primary' : "secondary"} ></Whatshot>
+                        }
+                        // <Button color='secondary' variant="contained" size='small' disableElevation
+                        sx={{
+                            // borderRadius: 10,
+                            // marginBottom:0.5
+                            width: 1,
+                            borderStartEndRadius: 0,
+                            borderStartStartRadius: 0
+                        }}>
+
+                        {proinfo.price}
+                    </Button>
+                </Tooltip>
+            </Paper>
+
             {/* 商品詳細画面(モーダル) */}
             <Modal open={mordalInfo} onClose={CloseMInfo}>
                 <ShowModalInfo product={proinfo} closeModal={CloseMInfo} />
--- src/components/3organisms/Products.tsx
+++ src/components/3organisms/Products.tsx
@@ -1,11 +1,13 @@
-import { LocalDrink }                               from '@mui/icons-material'
-import { CircularProgress, Grid, Modal, Paper }     from '@mui/material'
-import Product              from '../2molecules/Product'
-import { ProductTabs }      from '../2molecules/ProductTabs'
-import { ShowModalInfo }    from '../2molecules/ShowModalInfo'
+import { LocalDrink } from '@mui/icons-material'
+import { CircularProgress, Grid, Modal, Paper } from '@mui/material'
+import Product from '../2molecules/Product'
+import { ProductTabs } from '../2molecules/ProductTabs'
+import { ShowModalInfo } from '../2molecules/ShowModalInfo'
 import React, { useState } from 'react'
 import useSWR from 'swr'
 import { TypeProducts } from '../types/TypeProducts'
+import { useRecoilState } from 'recoil'
+import { cartState } from '../types/TypeCart'
 
 const fetchProduct = "/api/fetchProducts"
 const fetcher = (url: string) => fetch(url).then(response => response.json());
@@ -14,6 +16,7 @@ export const Products = () => {
 
     const { data, error } = useSWR<TypeProducts[]>(fetchProduct, fetcher);
     if (error) return (<>商品表示：エラーが発生しました。管理者へ連絡してください</>)
+    const [cart] = useRecoilState(cartState)
 
     return (
         // <Container>
@@ -31,6 +34,7 @@ export const Products = () => {
 
                                         <Product
                                             proinfo={data![j + 12 * i]}
+                                            cart={cart}
                                             key={data![j + 12 * i].id}
                                         />
                                         : <CircularProgress />}
