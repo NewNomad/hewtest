95e139f6712b18ccd7bfd48b41c031107a8626a9
aaaa <pocchaman0393@gmail.com>
2023-02-01 16:40:02 +0900

    design

--- src/components/2molecules/RecommendProduct.tsx
+++ src/components/2molecules/RecommendProduct.tsx
@@ -1,11 +1,13 @@
 import { AcUnit, Whatshot } from '@mui/icons-material'
-import { Badge, Button, Divider, Modal, Paper, Tooltip } from '@mui/material'
+import { Badge, Button, Divider, Modal, Paper, Tooltip, Grid } from '@mui/material'
 import { ShowModalInfo } from './ShowModalInfo'
 import { TypeProducts } from '../types/TypeProducts'
 import { cartState, TypeCart, useCart } from '../types/TypeCart'
 import Image from 'next/image'
 import { useRecoilState } from 'recoil'
 import React, { useState } from 'react'
+import { color, padding } from '@mui/system'
+import { relative } from 'path'
 
 // ----------------------------------------------------
 // 型宣言
@@ -35,142 +37,163 @@ export default function RecommendProduct({ proinfo, rank }: Props) {
     const quantity = cart.products.find((e, i) => e.id == id)?.quantity
 
     return (
-        <>
-            {/* 商品一覧画面 - 商品(一つ分)の表示 */}
-            <Paper elevation={3} variant="elevation" key={0}
-                sx={
-                    rank == 1 ?
-                        {
-                            backgroundColor: "#fff",
-                            border: `2px solid #ccc `,
-                            borderColor: `secondary.main`,
-                            textAlign: "center",
-                            width: 300,
-                            paddingTop: 5,
-                            marginRight: 20,
-                        }
-                        : rank == 2 || rank == 3 || rank == 4 ?
+        <Grid container spacing={0} padding={1} direction="row" >
+            <Grid item key={0} spacing={0} padding={0} direction="column" md={
+                rank == 1 ?
+
+                    12
+
+                    : rank == 2 || rank == 3 || rank == 4 ?
+
+                        9
+
+                        :
+                        0
+
+            }>
+                {/* 商品一覧画面 - 商品(一つ分)の表示 */}
+                < Paper elevation={3} variant="elevation" key={0}
+                    sx={
+                        rank == 1 ?
                             {
                                 backgroundColor: "#fff",
                                 border: `2px solid #ccc `,
                                 borderColor: `secondary.main`,
                                 textAlign: "center",
-                                width: 250,
-                                position: 'relative',
-                                top: 168,
-                                left: 55,
-
                             }
-                            : {
-                                width: 100,
-                            }
-                }>
-
-                {/* 画像部 */}
-                {/* TODO:[hoverアクション] 選択してるかどうか分からない。画像の透明度を下げるとか色を追加するとか追加する */}
-
-
-                <Tooltip title="この商品の詳細を見る" arrow>
-                    <Button
-                        onClick={OpenMInfo}
-                        disabled={!isActive}
-                        sx={{
-                            width: 1,
-                            borderBottomLeftRadius: 0,
-                            borderBottomRightRadius: 0,
-                        }}>
-                        {rank == 1 ?
-                            <Badge badgeContent={1} color="primary" max={9} sx={{
-                                position: "relative",
-                                top: -250,
-                                left: 30,
-                            }} 
-                            />
-                            :
-                            rank == 2 ?
-                                <Badge badgeContent={2} color="primary" max={9} sx={{
-                                    position: "relative",
-                                    top: -140,
-                                    left: 20,
-                                }} />
-                                : rank == 3 ?
-                                    <Badge badgeContent={3} color="primary" max={9} sx={{
-                                        position: "relative",
-                                        top: -140,
-                                        left: 20,
-                                    }} />
-                                    : rank == 4 ?
-                                        <Badge badgeContent={4} color="primary" max={9} sx={{
-                                            position: "relative",
-                                            top: -140,
-                                            left: 20,
-                                        }} />
-                                        : <></>
-                        }
-                        <Image
-                            src={"/" + imageURL}
-                            // height={400}
-                            width={400}
-                            objectFit="contain"
-                            style={!isActive ? { filter: "grayscale(100%)" } : {}}
-                            alt=""{
-                            ...rank == 1 ?
+                            : rank == 2 || rank == 3 || rank == 4 ?
                                 {
-                                    height: 622,
+                                    backgroundColor: "#fff",
+                                    border: `2px solid #ccc `,
+                                    borderColor: `secondary.main`,
+                                    textAlign: "center",
+                                    // display:"flex",
+                                    // justifyContent:"right",
+                                    // alignItems:"flex-end",
+                                    position:"relative",
+                                    top:75,
+                                    left:50,
 
                                 }
-                                : rank == 2 || rank == 3 || rank == 4 ?
+                                : {
+                                    width: 100,
+                                }
+                    }>
+
+                    {/* 画像部 */}
+                    {/* TODO:[hoverアクション] 選択してるかどうか分からない。画像の透明度を下げるとか色を追加するとか追加する */}
+
+
+                    <Tooltip title="この商品の詳細を見る" arrow>
+                        <Button
+                            onClick={OpenMInfo}
+                            disabled={!isActive}
+                            sx={{
+                                width: 1,
+                                borderBottomLeftRadius: 0,
+                                borderBottomRightRadius: 0,
+                            }}>
+                            {rank == 1 ?
+                                <Paper sx={{
+                                    backgroundColor: "#E45E00",
+                                    color: "#FFF",
+                                    fontSize: 30,
+                                    padding: 1,
+                                    width: 50,
+                                }}>1
+                                </Paper>
+                                :
+                                rank == 2 ?
+                                    <Paper sx={{
+                                        backgroundColor: "#E45E00",
+                                        color: "#FFF",
+                                        fontSize: 20,
+                                        padding: 1,
+                                        width: 50,
+                                    }}>2</Paper>
+                                    : rank == 3 ?
+                                        <Paper sx={{
+                                            backgroundColor: "#E45E00",
+                                            color: "#FFF",
+                                            fontSize: 20,
+                                            padding: 1,
+                                            width: 50,
+                                        }}>3</Paper>
+                                        : rank == 4 ?
+                                            <Paper sx={{
+                                                backgroundColor: "#E45E00",
+                                                color: "#FFF",
+                                                fontSize: 20,
+                                                padding: 1,
+                                                width: 50,
+                                            }}>4</Paper>
+                                            : <></>
+                            }
+                            <Image
+                                src={"/" + imageURL}
+                                height={400}
+                                width={400}
+                                objectFit="contain"
+                                style={!isActive ? { filter: "grayscale(100%)" } : {}}
+                                alt=""{
+                                ...rank == 1 ?
                                     {
-                                        height: 550,
+                                        // height: 622,
 
                                     }
-                                    : {
-                                        height: 200,
-                                    }
-                            } />
+                                    : rank == 2 || rank == 3 || rank == 4 ?
+                                        {
+                                            // height: 550,
 
-                    </Button>
-                </Tooltip>
+                                        }
+                                        : {
+                                            // height: 200,
+                                        }
+                                } />
 
-                <Divider />
+                        </Button>
+                    </Tooltip>
 
-                {/* 値段部 */}
-                {/* TODO:[hoverアクション] 選択してるかどうか分からない。画像部まで含めて変化 もしくｈｓ */}
-                <Tooltip title="この商品をカートに追加する" arrow>
+                    <Divider />
 
-                    <Button
-                        color='inherit'
-                        variant="contained"
-                        size='large'
-                        disableElevation
-                        onClick={() => addCart(proinfo)}
-                        disabled={!isActive}
-                        startIcon={
-                            isice == 1
-                                ? <AcUnit color={isActive ? "info" : "secondary"} />
-                                : <Whatshot color={isActive ? 'primary' : "secondary"} />
-                        }
-                        // <Button color='secondary' variant="contained" size='small' disableElevation
-                        sx={{
-                            // borderRadius: 10,
-                            // marginBottom:0.5
-                            width: 1,
-                            height: 80,
-                            borderStartEndRadius: 0,
-                            borderStartStartRadius: 0,
-                            fontSize: 30,
-                        }}>
-                        {price}
-                    </Button>
+                    {/* 値段部 */}
+                    {/* TODO:[hoverアクション] 選択してるかどうか分からない。画像部まで含めて変化 もしくｈｓ */}
+                    <Tooltip title="この商品をカートに追加する" arrow>
 
-                </Tooltip>
+                        <Button
+                            color='inherit'
+                            variant="contained"
+                            size='large'
+                            disableElevation
+                            onClick={() => addCart(proinfo)}
+                            disabled={!isActive}
+                            startIcon={
+                                isice == 1
+                                    ? <AcUnit color={isActive ? "info" : "secondary"} />
+                                    : <Whatshot color={isActive ? 'primary' : "secondary"} />
+                            }
+                            // <Button color='secondary' variant="contained" size='small' disableElevation
+                            sx={{
+                                // borderRadius: 10,
+                                // marginBottom:0.5
+                                width: 1,
+                                height: 80,
+                                borderStartEndRadius: 0,
+                                borderStartStartRadius: 0,
+                                fontSize: 30,
+                            }}>
+                            {price}
+                        </Button>
 
-            </Paper>
+                    </Tooltip>
+
+                </Paper >
+            </Grid >
 
             {/* 商品詳細画面(モーダル) */}
-            <Modal open={mordalInfo} onClose={CloseMInfo}>
+            <Modal open={mordalInfo} onClose={CloseMInfo} >
                 <ShowModalInfo product={proinfo} closeModal={CloseMInfo} />
             </Modal>
-        </>
+        </Grid>
     )
 }
