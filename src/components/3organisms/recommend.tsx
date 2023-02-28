559fe4a342eeae76a710a87ce3fe43a5ef25a8ce
NewNomad <tosiki1997@gmail.com>
2023-02-28 23:24:26 +0900

    add:face-api, webcam

--- src/components/3organisms/recommend.tsx
+++ src/components/3organisms/recommend.tsx
@@ -1,58 +1,62 @@
 import { LocalDrink, RecommendOutlined } from '@mui/icons-material'
-import { CircularProgress, Container, Grid, Modal, Paper, Tab, Tabs, Typography } from '@mui/material'
+import { Button, CircularProgress, Container, Grid, Modal, Paper, Tab, Tabs, Typography } from '@mui/material'
 import Product from '../2molecules/Product'
 import { ProductTabs } from '../2molecules/ProductTabs'
 import { ShowModalInfo } from '../2molecules/ShowModalInfo'
 import { TypeProducts } from '../types/TypeProducts'
 import { cartState } from '../types/TypeCart'
 import { useRecoilState } from 'recoil'
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import useSWR from 'swr'
 import RecommendProduct from '../2molecules/RecommendProduct'
+import Webcam from 'react-webcam'
+import * as faceapi from "face-api.js"
 
 const fetchProduct = "/api/fetchProducts"
 const fetcher = (url: string) => fetch(url).then(response => response.json());
 
 
 export const Recommend = () => {
+    const webcamRef = useRef<Webcam>(null)
 
-    const [tabval, settabval] = useState<number>(0)
-    const { data, error } = useSWR<TypeProducts[]>(fetchProduct, fetcher);
-    const [cart] = useRecoilState(cartState)
-
-    const handleTabVal = (event: any, newtabindex: number) => {
-        settabval(newtabindex)
+    const loadModels = async () => {
+        const MODEL_URL = "/weights"
+        await Promise.all([
+            faceapi.nets.tinyFaceDetector.load(MODEL_URL),
+            faceapi.nets.ageGenderNet.load(MODEL_URL),
+            // faceapi.nets.faceExpressionNet.load(MODEL_URL)
+        ])
     }
 
-    if (error) return (<>商品表示：エラーが発生しました。管理者へ連絡してください</>)
+    const faceDetectHandler = async () => {
+        await loadModels()
+        if (webcamRef.current) { // + canvas 
+            const webcam = webcamRef.current.video as HTMLVideoElement
+            const video = webcamRef.current.video as HTMLVideoElement
+            const detectionsWithGenderNet = await faceapi
+                .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+                .withAgeAndGender();
+            return detectionsWithGenderNet
+
+        }
+    }
+
+    useEffect(() => {
+        const timer = setInterval(async () => {
+            const detectionsWithGenderNet = await faceDetectHandler()
+            console.log(detectionsWithGenderNet);
+        }, 1000)
+        return () => clearInterval(timer)
+    }, [])
+
+
 
     return (
         <>
-            <Grid container direction="column" spacing={0.5} padding={3} sx={{backgroundColor:"#FFA00"}}>
-                {tabval == 0 ?
-                    [...Array(1)].map((e, i) => (
-
-                        <Grid key={i} item xs={4} sx={{ height: "100%" }} container spacing={0.5} padding={0} paddingTop={0} paddingBottom={0}>
-
-                            {[...Array(4)].map((item, j) => (
-                                <Grid key={j} item xs={3}>
-                                    {data ?
-                                            <RecommendProduct
-                                                proinfo={data![j + 12 * i]}
-                                                cart={cart}
-                                                key={data![j + 12 * i].id}
-                                                rank={j+1}
-                                            />
-                                        :
-                                        <CircularProgress />
-                                    }
-                                </Grid>
-                            ))}
-                        </Grid>
-                    ))
-                    : <Typography>おすすめ準備中</Typography>
-                }
-            </Grid>
+            <Webcam
+                audio={false}
+                ref={webcamRef}
+            />
         </>
     )
 }
